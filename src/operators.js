f193b407c595ab907d8792c6cac920a589ec6438
plowww <kevin_g@my.uri.edu>
2022-08-31 17:33:20 -0400

    Adding operators.js and the script tag

---
+++ src/operators.js
@@ -0,0 +1,24 @@
+show('Look at operators.js to see how this page is setup.');
+// Switch statement
+show('Switch statement')
+let grade = 'C';
+switch(grade) {
+  case 'A':
+    show('You got an A!');
+    break;
+  case 'B':
+    show('You got a B!');
+    break;
+  case 'C':
+    show('You got a C!');
+    break;
+  case 'D':
+    show('You got a D!');
+    break;
+  default:
+    show('You...did not pass. :(');
+    break;
+}
+
+// for in/for of loops
+let forInObj = {};
