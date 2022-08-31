cc6617be6902377bd441f0656f1e2501469d1d95
plowww <kevin_g@my.uri.edu>
2022-08-31 18:15:53 -0400

    Changing let to const to better reflect intent

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@
  * We create an h1 here to do so, in the 'content' portion
  */
 (function setupHome() {
-  let contentArea = document.getElementById('content');
-  let intro = document.createElement('h1');
+  const contentArea = document.getElementById('content');
+  const intro = document.createElement('h1');
   
   intro.innerHTML = "Welcome to the home page! To see how this was setup, look at utils.js and index.js" +
   ", otherwise navigate to any page you wish to view snippets from what I've learned from lessons on Pluralsight";
