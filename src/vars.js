c17a6b002334b291017e3461766ba7ff6c6b338f
plowww <kevin_g@my.uri.edu>
2022-09-02 15:36:49 -0400

    First part of 2 done

--- src/vars.js
+++ src/vars.js
@@ -8,3 +8,106 @@
  * Controls what "this" means in certain contexts
  */
 show("Look at vars.js to see how this page is setup.", "h1");
+
+// Template Literal
+show(tempLit('sampleString'));
+
+// Tagged template literal
+show(taggedTempLit());
+
+// Let VS Const
+show(letVsConst());
+
+/**
+ * This just returns a template literal
+ * @param {string} string The sample string to display
+ * @returns String
+ */
+function tempLit(string) {
+  return `Template Literal: Param passed was ${string}`;
+}
+
+/**
+ * This is the tagger function that tags a template literal,
+ * making each value in it italic
+ * @param {string} strings The strings being passed
+ * @param  {...any} values The values evaluated in the temp literal
+ * @returns Resulting string with modifications made to values
+ */
+function italics(strings, ...values) {
+  let result = "";
+  // For every string in the literal:
+  /*
+    * If we are passed the first one (since the first is always a string)
+      * Add <i> and </i> around the evaluated values[i-1] (starts at 0)
+    * Add the original string at index i (starts at 0)
+  */
+ // Then return result string
+  for (let i = 0; i < strings.raw.length; i++) {
+    if (i > 0) {
+      result += `<i>${values[i-1]}</i>`;
+    }
+    result += strings.raw[i]
+  }
+  return result;
+}
+
+/**
+ * This reutnrs a tagged template literal using 'italics'
+ * @returns {string} Template literal tagged with italics
+ */
+function taggedTempLit() {
+  let str1 = 'italic';
+  let str2 = 'variable';
+  let str3 = 'evaluated';
+  return italics `Tagged Template Literal: This template lit
+   is ${str1} for every ${str2} that is ${str3}`;
+}
+
+/**
+ * Shows constraints between let and const. We do not show scope,
+ * but scope is of course block scope.
+ * @returns Resulting string after all try catch blocks have been done
+ */
+function letVsConst() {
+  let x = 3, y = 3;
+  const mul = 4;
+  const constObj = {name: 'paul',age: 22};
+  let result = `Let VS Const: Original vars are 
+  x: ${x}, y: ${y}, mul: ${mul}, constObj: ${JSON.stringify(constObj)} `;
+  try {
+    result += `Attempting to change x and y: `;
+    x = 4;
+    y = 6;
+    result += `x: ${x}, y: ${y} `;
+  }
+  catch (e) {
+    result += e + ' ';
+  }
+  try {
+    result += `Attempting to change const mul: `;
+    mul = 4;
+    result += `mul: ${mul} `;
+  }
+  catch (e) {
+    result += e + ' '
+  }
+  try {
+    result += `Attempting to change const constObj: `;
+    constObj = {name: 'sam',age: 22};
+    result += `constObj: ${JSON.stringify(constObj)} `;
+  }
+  catch (e) {
+    result += e + ' ';
+  }
+  try {
+    result += `Attempting to change property of obj referenced by const constObj: `;
+    constObj.age = 9;
+    result += `constObj.age: ${constObj.age} `;
+  }
+  catch (e) {
+    result += e + ' ';
+  }
+  return result;
+}
+
