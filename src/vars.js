c23b4a796aef349f4be583080b48f7f4108ab561
plowww <kevin_g@my.uri.edu>
2022-09-02 13:11:17 -0400

    Adding template for variables and types

---
+++ src/vars.js
@@ -0,0 +1,10 @@
+'use strict';
+// Use strict ensures the following:
+/*
+ * Cannot use variable without var or let or const keywords
+ * Cannot use reserved words like eval or arguments for variables
+ * Cannot delete a variable
+ * Cannot delete a function
+ * Controls what "this" means in certain contexts
+ */
+show("Look at vars.js to see how this page is setup.", "h1");
