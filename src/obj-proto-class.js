5e0a746c6d46fd8826d5dff5c0f5e7b447bee25c
plowww <kevin_g@my.uri.edu>
2022-09-09 06:41:09 -0400

    Template for obj-proto-class

---
+++ src/obj-proto-class.js
@@ -0,0 +1,12 @@
+'use strict';
+// Use strict ensures the following:
+/*
+ * Cannot use variable without var or let or const keywords
+ * Cannot use reserved words like eval or arguments for variables
+ * Cannot delete a variable
+ * Cannot delete a function
+ * Controls what "this" means in certain contexts
+ */
+
+show("Look at obj-proto-class.js to see how this page is setup.", "h1");
+
