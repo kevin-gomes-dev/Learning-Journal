9332baa6886f31b88e130daef2c4f7bb9eed28eb
plowww <kevin_g@my.uri.edu>
2022-09-10 20:41:38 -0400

    Updating index.js to use show function

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@
  * We create an h1 here to do so, in the 'content' portion
  */
 (function setupHome() {
-  const contentArea = document.getElementById('content');
-  const intro = document.createElement('h1');
-  
-  intro.innerHTML = "Welcome to the home page! To see how this was setup, look at utils.js and index.js" +
-  ", otherwise navigate to any page you wish to view snippets from what I've learned from lessons on Pluralsight";
-
-  contentArea.appendChild(intro);
+  show(`Welcome to the home page! To see how this was setup, look at utils.js and index.js,
+  otherwise navigate to any page you wish to view snippets from what I've learned from
+  lessons on Pluralsight`,'h1');
 })();
