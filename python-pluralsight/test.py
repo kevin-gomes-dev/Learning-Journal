9a3138f6d6d7b8a76cc64a86728e884dd3eccec5
kevin <kevin.gomes.dev@gmail.com>
2025-03-30 11:41:37 -0400

    fixing up test

--- python-pluralsight/test.py
+++ python-pluralsight/test.py
@@ -1,48 +1,49 @@
 import os
 
-
-def find_acro(acro):
-    with open('test.txt','r') as file:
-        found = False
+# Returns list of acro,definition pair if found, otherwise False
+def find_acro(path,acro):
+    with open(path,'r') as file:
         for line in file:
-            acro_pair = line.split("-")
+            acro_pair = line.strip().split("-")
             if acro_pair[0].strip().upper() == str(acro.upper()):
-                found = True
-                print(acro_pair[1].strip())
-                break
-    if not found:
-        print(f'The acronym given, {acro}, does not exist.')
+                return acro_pair
+        return False
 
-def add_acro(acro,defi):
+# Returns True if added, False if not
+def add_acro(path,acro,defi):
     if acro == "" or defi == "":
-        print("Either acronym or definition was blank.")
-        return
-    with open('test.txt','a+') as file:
+        return False
+    with open(path,'a') as file:
         file.write('\n')
         str_to_write = f'{acro} - {defi}'
         file.write(str_to_write)
-    print(f'Successfully wrote acronym {acro} and definition {defi}')
-    
+        return True
+
+# Returns False if no path given
 def acro_program():
+    path = input("Enter path including filename: ")
+    if not path:
+        return False
     while True:
-        inp = input("Enter mode. 1 for find, 2 for add: ")
-        if inp == "":
+        mode = input("Enter mode. 1 for find, 2 for add: ")
+        if mode == "":
             break
-        if inp == "1":
+        if mode == "1":
             acro = input("Enter acronym: ")
-            find_acro(acro)
-        elif inp == "2":
+            acro_pair = find_acro(path,acro)
+            print(acro_pair) if acro_pair else print(f'The acronym given, {acro}, does not exist.')
+        elif mode == "2":
             acro = input("Enter acronym: ")
             defi = input("Enter definition: ")
-            add_acro(acro,defi)
+            status = add_acro(path,acro,defi)
+            print(f'Success: {acro} - {defi}') if status else print(f'Fail: {acro} - {defi}')
         else:
             print("Input given doesn't correspond to mode.")
         print()
 
-def move_subs():
-    path = input("Enter path\n")
+# Moving a bunch of sub files from various folders into centralized one
+def move_subs(path):
     path = os.path.normpath(os.path.realpath(path))
-
     dirs = os.walk(path)
     for i,j,k in dirs:
         # Get S/E
@@ -53,8 +54,6 @@ def move_subs():
                 new_name = str(num).zfill(2) + "_eng.ass"
                 old_path = i + "\\track4_eng.ass"
                 new_path = i + "\\" + new_name
-                # old_path = ""
-                # new_path = ""
                 print("OLD:",old_path)
                 print("NEW:",new_path)
                 # Uncomment when ready to rename/move
@@ -63,9 +62,17 @@ def move_subs():
             except:
                 print("Couldn't print int of:",x)
         print("===================================")
-    
+
+# Load any function with params
 def main():
-    acro_program()
+    choice = input("Give name of function. If params, separate with ,. Like so - multiply_2,5,6\n")
+    fixed_choice = choice.strip().split(',')
+    func = fixed_choice[0]
+    args = fixed_choice[1:] if len(fixed_choice) > 1 else []
+    # Call function with params
+    if func in globals():
+        globals()[func](*args)
+    
     
 if __name__ == "__main__":
     main()
